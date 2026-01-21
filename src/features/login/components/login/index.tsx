f5f87f22f525fbba8ef40849c75155b776edead4
Daria <deimoses04@gmail.com>
2026-01-21 11:24:14 +0300

    [#0009]: LoginForm refactoring, added useAuth custom hook

--- src/features/login/components/login/index.tsx
+++ src/features/login/components/login/index.tsx
@@ -1,68 +1,28 @@
-import { useLoginUserMutation, useRegisterUserMutation } from 'src/store/api/baseApi';
 import { LoginForm } from '../../atoms/login-form';
 import { IUserApiBody } from 'src/types/store';
-import { handleCreateUserToken } from '../../helpers';
-import { useDispatch } from 'react-redux';
-import { saveUserData } from 'src/store/slices/user/user-slice';
-import { Alert } from 'react-native';
 import { useState } from 'react';
+import { useAuth } from '../../hooks';
 
 export const Login = () => {
   const [isSignUpScreen, setIsSignUpScreen] = useState<boolean>(true);
-  const [registerUser, { isLoading }] = useRegisterUserMutation();
-
-  const [loginUser] = useLoginUserMutation();
-  const dispatch = useDispatch();
-
-  const handleSignUp = async (userData: IUserApiBody) => {
-    try {
-      const { token, user } = await registerUser(userData).unwrap();
-      if (token) {
-        await handleCreateUserToken(token);
-        dispatch(saveUserData({ name: user.name, id: user.id }));
-      }
-    } catch (error: any) {
-      Alert.alert('Ошибка', error.message);
-      console.log(error, 'error registerUser');
-    }
-  }
-    const handleSignIn = async (userData: IUserApiBody) => {
-    try {
-      const { token, user } = await loginUser(userData).unwrap();
-      if (token) {
-        await handleCreateUserToken(token);
-        dispatch(saveUserData({ name: user.name, id: user.id }));
-      }
-    } catch (error: any) {
-      Alert.alert('Ошибка', error.message);
-      console.log(error, 'error registerUser');
-    }
-  }
+  const { handleAuth, isLoading } = useAuth();
 
   const handleUserAuthentication = async (name: string, password: string) => {
     const userData: IUserApiBody = {
       name: name,
       password: password,
     };
-    try {
-        if (isSignUpScreen) {
-            await handleSignUp(userData)
-        } else {
-            await handleSignIn(userData)
-        }
-    } catch (error) {
-        console.log(error, 'Error handleUserAuthentication')
-    }
+    await handleAuth(userData, isSignUpScreen);
   };
 
   const handleChangeForm = () => setIsSignUpScreen(!isSignUpScreen);
   return (
-      <LoginForm
-        isSignUpScreen={isSignUpScreen}
-        formHeader={isSignUpScreen ? 'Регистрация' : 'Вход'}
-        handleChangeForm={handleChangeForm}
-        handleUserAuthentication={handleUserAuthentication}
-        isLoading={isLoading}
-      />
+    <LoginForm
+      isSignUpScreen={isSignUpScreen}
+      formHeader={isSignUpScreen ? 'Регистрация' : 'Вход'}
+      handleChangeForm={handleChangeForm}
+      handleUserAuthentication={handleUserAuthentication}
+      isLoading={isLoading}
+    />
   );
 };
