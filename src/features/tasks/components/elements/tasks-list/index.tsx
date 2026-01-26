4066c343465cf2f09d1e5dc59884aad169bd11a3
Daria Zakharova <99590272+lunaaur@users.noreply.github.com>
2026-01-26 15:07:51 +0300

    [#0014]: Added completeTask mutation

--- src/features/tasks/components/elements/tasks-list/index.tsx
+++ src/features/tasks/components/elements/tasks-list/index.tsx
@@ -1,42 +1,49 @@
-import { useGetTasksQuery } from 'src/store/api/tasksApi';
+import { useCallback } from 'react';
+import { useCompleteTaskMutation, useGetTasksQuery } from 'src/store/api/tasksApi';
 import { SingleTask } from '../../atoms/single-task';
 import { ITaskState } from 'src/types/task';
 import { List } from './styled';
 import { LineSeparator } from '@atoms';
 
-import { FormatEnum, handleDateFormation } from 'src/shared/helpers/formatDate';
-
 export const TasksList = () => {
   const { data } = useGetTasksQuery();
+  
+  const [ completeTask ] = useCompleteTaskMutation();
 
-  const handleCompleteTask = async ({ item }: { item: ITaskState }) => {
+  const handleCompleteTask = useCallback(async ({ id, completed }: { 
+    id: number, 
+    completed: boolean 
+  }): Promise<boolean> => {
     try {
-      console.log(item, 'item');
-      //* some logic with api for single task
+      await completeTask({id: id, completed: !completed}).unwrap();
       return true;
     } catch (error: any) {
       console.log(error, 'Error handlePress');
+      return false;
     }
-  };
+  }, [completeTask]);
 
-  const renderItem = ({ item }: { item: ITaskState }) => {
+  const renderItem = useCallback(({ item }: { item: ITaskState }) => {
 
     return (
       <SingleTask
         taskName={item.name}
         isCompleted={item.completed}
-        creationDate={handleDateFormation({date: item.created_at, dateFormat: FormatEnum.DATEMONTH})}
-        handlePress={() => handleCompleteTask({ item })}
+        creationDate={item.created_at}
+        handlePress={() => handleCompleteTask({ 
+          id: item.id, 
+          completed: item.completed 
+        })}
       />
     );
-  };
+  }, [handleCompleteTask]);
 
   return (
-  <List
-    keyExtractor={(item: ITaskState) => item.id.toString()}
-    data={data?.tasks}
-    renderItem={renderItem}
-    ItemSeparatorComponent={() => <LineSeparator />}
+    <List
+      keyExtractor={(item: ITaskState) => item.id.toString()}
+      data={data?.tasks}
+      renderItem={renderItem}
+      ItemSeparatorComponent={ () => <LineSeparator />}
     />
-);
+  );
 };
--- src/store/slices/tasks/tasks-slice.ts
+++ src/store/slices/tasks/tasks-slice.ts
@@ -1,6 +1,12 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ITaskState } from 'src/types/task';
 
-const initialState = {
+export interface ITasksState {
+  count: number;
+  tasks: ITaskState[];
+}
+
+const initialState: ITasksState = {
   count: 0,
   tasks: [],
 };
@@ -9,13 +15,28 @@ export const tasksSlice = createSlice({
   name: 'tasksData',
   initialState,
   reducers: {
-    getTasks: (state, action) => {
+    getTasks: (
+      state,
+      action: PayloadAction<{ count: number; tasks: ITaskState[] }>,
+    ) => {
       state.count = action.payload.count;
       state.tasks = action.payload.tasks;
+    },
+    completeTask: (
+      state,
+      action: PayloadAction<{ id: number; completed: boolean }>,
+    ) => {
+      const tasks = state.tasks;
+      const modifiedTasks = tasks.map((task: ITaskState) => {
+        if (task.id && task.id === action.payload.id) {
+          return { ...task, completed: action.payload.completed };
+        } else return task;
+      });
+      state.tasks = modifiedTasks;
     },
   },
 });
 
-export const { getTasks } = tasksSlice.actions;
+export const { getTasks, completeTask } = tasksSlice.actions;
 
 export default tasksSlice.reducer;
